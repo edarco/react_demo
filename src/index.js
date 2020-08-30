7f17fbd4d212a512b34ece108bc2abfa7d20f3f0
edarco <edarco.net@gmail.com>
2020-08-30 18:43:37 +0400

    Lesson 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,46 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+
+
+
+// import hello from "./demo/example";
+// import {sum} from "./demo/example";
+// import {name} from "./demo/example";
+
+// import hello, {sum,name} from "./demo/example";
+// hello();
+
+// import greeting, {sum,name} from "./demo/example";
+// import greeting, {sum, name as newName} from "./demo/example";
+
+// greeting(); // this is default hello
+
+
+
+
+// sum(1,2);
+// console.log(name);
+// console.log(newName);
+
+
+
+import * as properties from "./demo/example";
+// console.log(properties);
+// console.log(properties.default());
+console.log(properties.sum(15,6));
+
+
+
+
+
+
+
+
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
