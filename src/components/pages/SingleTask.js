c1d8a94bd56e075e9541824e7439a38f65cf5ce8
edarco <57370463+edarco@users.noreply.github.com>
2020-11-15 15:42:39 +0400

    Lesson 19 (#4)

---
+++ src/components/pages/SingleTask.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+class SingleTask extends PureComponent {
+   
+    render() {
+        
+        return (
+            <div>
+                This is single task page.
+            </div>
+        );
+    }
+}
+
+export default SingleTask;
