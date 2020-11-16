b26996afadd7e6b09b2db89cfe52caf3265b54e6
edarco <57370463+edarco@users.noreply.github.com>
2020-11-16 10:38:20 +0400

    Lesson 20 (#5)

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import {Link} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function NavMenu() {
 
     return (
         <Navbar bg="primary" variant="dark">
-            <Link to='/'><Navbar.Brand>Home </Navbar.Brand></Link>
+            <Navbar.Brand>
+                <NavLink
+                    exact
+                    activeClassName='activeLink'
+                    to='/'
+                    className='navLink'
+                >
+                    Home
+                </NavLink>
+            </Navbar.Brand>
             <Nav className="mr-auto">
-                <Link to='/task' className='nav-link'>Task</Link>
-                <Nav.Link href="https://reactrouter.com/web/guides/quick-start" target="_blank">Go to documentation</Nav.Link>
+                <NavLink
+                    activeClassName='activeLink'
+                    to='/task'
+                    className='navLink'
+                >
+                    Task
+                </NavLink>
+                <Nav.Link
+                    href="https://reactrouter.com/web/guides/quick-start"
+                    target="_blank"
+                >
+                    Go to documentation
+                </Nav.Link>
             </Nav>
         </Navbar>
     );
