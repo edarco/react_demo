8470b649228c5fcfd968a2cce685498c7dbffd5b
edarco <edarco.net@gmail.com>
2021-01-08 16:48:55 +0400

    lesson-31

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 export default function NotFound() {
-    return(
-        <div>Page not found</div>
+    return (
+        <Card className='text-center'>
+            <Card.Body>
+                <Card.Text>Page not found!</Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
