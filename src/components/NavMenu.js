120d18a28782519526c841d7cf163b0f7a177821
edarco <57370463+edarco@users.noreply.github.com>
2021-01-09 13:44:02 +0400

    Lesson 32 (#18)

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -32,11 +32,18 @@ export default function NavMenu() {
                 >
                     Task
                 </NavLink>
+                <NavLink
+                    activeClassName='activeLink'
+                    to='/register'
+                    className='navLink'
+                >
+                    Register
+                </NavLink>
                 <Nav.Link
                     href="https://reactrouter.com/web/guides/quick-start"
                     target="_blank"
                 >
-                    Go to documentation
+                    Documentation
                 </Nav.Link>
             </Nav>
         </Navbar>
