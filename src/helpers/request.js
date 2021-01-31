f637b52cfe536d9d68fdd8e072dcbb80a982a10f
edarco <57370463+edarco@users.noreply.github.com>
2021-01-31 21:09:42 +0400

    Lesson 40 (#29)

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -2,20 +2,24 @@ import { getJWT } from './auth';
 
 const defaultError = { message: 'Something went wrong!' };
 
-async function request(url, method = 'GET', body) {
-    const jwt = await getJWT();
-
-    if (!jwt) {
-        return Promise.reject(defaultError);
-    }
+async function request(url, method = 'GET', body, checkJwt = true) {
 
     const config = {
         method: method,
         headers: {
             "Content-Type": 'application/json',
-            "Authorization": `Bearer ${jwt}`
         }
     };
+
+    if (checkJwt) {
+        const jwt = await getJWT();
+
+        if (!jwt) {
+            return Promise.reject(defaultError);
+        }
+
+        config.headers["Authorization"] = `Bearer ${jwt}`;
+    }
 
     if (body) {
         config.body = JSON.stringify(body);
